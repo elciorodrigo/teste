c7d112d8d00d53addfc5f2538b166c3760846da1
elciorodrigo <elciorodrigo@gmail.com>
2016-02-14 13:21:44 -0200

    firt delpoy

---
+++ Desktop/logatti/src/main/java/logatti/Boot.java
@@ -0,0 +1,14 @@
+package logatti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+}
